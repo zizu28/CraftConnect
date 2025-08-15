e5b971d6e007e4ab156cf710f0d680e163b63319
Zulka <ziblimzulka.zz@gmail.com>
2025-08-16 03:20:36 +0700

    Added BookingUpdatedIntegrationEvent

---
+++ Core.SharedKernel/IntegrationEvents/BookingUpdatedIntegrationEvent.cs
@@ -0,0 +1,20 @@
+﻿using Core.SharedKernel.Domain;
+using Core.SharedKernel.Enums;
+using Core.SharedKernel.ValueObjects;
+
+namespace Core.SharedKernel.IntegrationEvents
+{
+	public record BookingUpdatedIntegrationEvent(
+		Guid BookingId,
+		Guid CustomerId,
+		Guid CraftsmanId,
+		BookingStatus Status,
+		DateTimeRange? Duration,
+		decimal TotalPrice,
+		DateTime UpdatedAt) : IIntegrationEvent
+	{
+		public Guid EventId => Guid.NewGuid();
+
+		public DateTime OccuredOn => DateTime.UtcNow;
+	}
+}
