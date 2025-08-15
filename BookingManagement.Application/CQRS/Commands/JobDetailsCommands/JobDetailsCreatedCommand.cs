3155d79c9712199f7a5aa4ee038fbfc514f71d05
Zulka <ziblimzulka.zz@gmail.com>
2025-08-16 05:32:20 +0700

    Add commands and events for booking and craftsmen
    Introduce `BookingLineItemCreateCommand` for managing booking line items with properties for `BookingId` and `LineItemCreateDTO`. Add integration events: `CraftmanSkillIntegrationAddedEvent`, `CraftmanVerifiedIntegrationEvent`, and `UserRegisteredIntegrationEvent`, each with relevant properties and automatic timestamp generation. These changes improve the application's functionality for handling bookings and tracking craftsmen-related events.

---
+++ BookingManagement.Application/CQRS/Commands/JobDetailsCommands/JobDetailsCreatedCommand.cs
@@ -0,0 +1,11 @@
+﻿using BookingManagement.Application.DTOs.BookingLineItemDTOs;
+using MediatR;
+
+namespace BookingManagement.Application.CQRS.Commands.BookingCommands.JobDetailsCommands
+{
+	public class BookingLineItemCreateCommand : IRequest<BookingLineItemResponseDTO>
+	{
+		public Guid BookingId { get; set; }
+		public BookingLineItemCreateDTO LineItemCreateDTO { get; set; }
+	}
+}
