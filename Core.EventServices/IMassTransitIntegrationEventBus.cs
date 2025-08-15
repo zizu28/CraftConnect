d0b857b45f0a309bf0090306c79bd907e0e3c548
Zulka <ziblimzulka.zz@gmail.com>
2025-08-16 03:22:02 +0700

    Deleted IMassTransitIntegrationEvent.cs interface and MassTransitIntegrationEvent.cs implementation

--- Core.EventServices/IMassTransitIntegrationEventBus.cs
+++
@@ -1,9 +0,0 @@
-﻿using Core.SharedKernel.Domain;
-
-namespace Core.EventServices
-{
-	public interface IMassTransitIntegrationEventBus
-	{
-		Task PublishAsync<T>(T @event, CancellationToken cancellationToken = default) where T : class, IIntegrationEvent;
-	}
-}
