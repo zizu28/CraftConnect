e99e233efbd668cf8171d8cb85b234e427b00a22
Zulka <ziblimzulka.zz@gmail.com>
2025-08-19 01:07:34 +0700

    Refactor API Gateway and enhance service configurations
    - Removed Controllers folder inclusion in Core.APIGateway.csproj and added project reference to Core.Logging.
    - Updated Program.cs to load Ocelot configuration from JSON, added Fluent Email and Serilog services, and commented out authentication/authorization setup.
    - Modified appsettings.json to include new JWT settings, Redis and RabbitMQ connection strings, and MassTransit configuration.
    - Enhanced ocelot.json with routes for user and booking management services, including rate limiting options.

--- Core.APIGateway/Program.cs
+++ Core.APIGateway/Program.cs
@@ -1,4 +1,6 @@
+using Core.Logging;
 using Infrastructure.BackgroundJobs;
+using Infrastructure.EmailService;
 using Infrastructure.Persistence.Data;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
@@ -14,42 +16,53 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Configuration.AddOcelot(builder.Environment.ContentRootPath);
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
 builder.Services.AddBackgroundJobs(builder.Configuration);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 	options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
-builder.Services.AddAuthentication(options =>
-{
-	options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-	options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-	.AddJwtBearer(options =>
-	{
-		options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
-		{
-			ValidateIssuer = true,
-			ValidateAudience = true,
-			ValidateLifetime = true,
-			ValidateIssuerSigningKey = true,
-			ValidIssuer = builder.Configuration["Jwt:Issuer"],
-			ValidAudience = builder.Configuration["Jwt:Audience"],
-			IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-		};
-	});
+//builder.AddServiceDefaults();
+//builder.AddRabbitMQClient("rabbitmq");
+// Add services to the container.
 
-builder.Services.AddAuthorization(options =>
-{
-	options.FallbackPolicy = new AuthorizationPolicyBuilder()
-		.AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
-		.RequireAuthenticatedUser()
-		.Build();
-	options.AddPolicy("ApiScope", policy =>
-	{
-		policy.RequireAuthenticatedUser();
-		policy.RequireClaim("scope", "api1");
-	});
-});
+builder.Host.ConfigureSerilog();
+builder.Services.AddFluentEmailService(builder.Configuration);
+//builder.Services.AddHybridCacheService(builder.Configuration);
+builder.Services.AddBackgroundJobs(builder.Configuration);
+builder.Services.RegisterSerilog();
+
+//builder.Services.AddAuthentication(options =>
+//{
+//	options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+//	options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+//})
+//	.AddJwtBearer(options =>
+//	{
+//		options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+//		{
+//			ValidateIssuer = true,
+//			ValidateAudience = true,
+//			ValidateLifetime = true,
+//			ValidateIssuerSigningKey = true,
+//			ValidIssuer = builder.Configuration["Jwt:Issuer"],
+//			ValidAudience = builder.Configuration["Jwt:Audience"],
+//			IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+//		};
+//	});
+
+//builder.Services.AddAuthorization(options =>
+//{
+//	options.FallbackPolicy = new AuthorizationPolicyBuilder()
+//		.AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+//		.RequireAuthenticatedUser()
+//		.Build();
+//	options.AddPolicy("ApiScope", policy =>
+//	{
+//		policy.RequireAuthenticatedUser();
+//		policy.RequireClaim("scope", "api1");
+//	});
+//});
 
 var app = builder.Build();
 
