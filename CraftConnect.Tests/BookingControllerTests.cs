02cce4abddb180e1eb193ecfee1beb1ac86c700d
Zulka <ziblimzulka.zz@gmail.com>
2025-08-20 08:53:49 +0700

    chore: restore CraftConnect.Tests project and test files to original structure

---
+++ CraftConnect.Tests/BookingControllerTests.cs
@@ -0,0 +1,347 @@
+﻿using BookingManagement.Application.CQRS.Commands.BookingCommands;
+using BookingManagement.Application.CQRS.Queries.BookingQueries;
+using BookingManagement.Presentation.Controllers;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace CraftConnect.Tests
+{
+	public class BookingsControllerTests
+	{
+		private readonly Mock<IMediator> _mediatorMock;
+		private readonly BookingsController _bookingsController;
+
+		public BookingsControllerTests()
+		{
+			_mediatorMock = new Mock<IMediator>();
+			_bookingsController = new BookingsController(_mediatorMock.Object);
+		}
+
+		[Fact]
+		public async Task GetBookingByIdAsync_ReturnsOk_WhenBookingExists()
+		{
+			// Arrange
+			var bookingId = Guid.NewGuid();
+			var bookingQuery = new GetBookingByIdQuery();
+			_mediatorMock.Setup(m => m.Send(It.IsAny<GetBookingByIdQuery>(), default))
+				.ReturnsAsync(bookingQuery);
+
+			// Act
+			var result = await _bookingsController.GetBookingByIdAsync(bookingId);
+
+			// Assert
+			var okResult = Assert.IsType<OkObjectResult>(result);
+			Assert.Equal(bookingQuery, okResult.Value);
+		}
+
+		[Fact]
+		public async Task GetBookingByIdAsync_ReturnsNotFound_WhenBookingDoesNotExist()
+		{
+			// Arrange
+			var bookingId = Guid.NewGuid();
+			_mediatorMock.Setup(m => m.Send(It.IsAny<GetBookingByIdQuery>(), default)).ReturnsAsync((GetBookingByIdQuery)null);
+
+			// Act
+			var result = await _bookingsController.GetBookingByIdAsync(bookingId);
+
+			// Assert
+			Assert.IsType<NotFoundObjectResult>(result);
+		}
+
+		[Fact]
+		public async Task GetBookingByIdAsync_ReturnsBadRequest_WhenIdIsEmpty()
+		{
+			// Arrange
+			var bookingId = Guid.Empty;
+
+			// Act
+			var result = await _bookingsController.GetBookingByIdAsync(bookingId);
+
+			// Assert
+			Assert.IsType<BadRequestObjectResult>(result);
+		}
+
+		[Fact]
+		public async Task GetAllBookingAsync_ReturnsOk_WhenBookingsExist()
+		{
+			// Arrange
+			var bookings = new List<GetAllBookingsQuery> { new GetAllBookingsQuery(), new GetAllBookingsQuery() };
+			_mediatorMock.Setup(m => m.Send(It.IsAny<GetAllBookingsQuery>(), default)).ReturnsAsync(bookings);
+
+			// Act
+			var result = await _bookingsController.GetAllBookingAsync();
+
+			// Assert
+			var okResult = Assert.IsType<OkObjectResult>(result);
+			var returnedBookings = Assert.IsAssignableFrom<IEnumerable<GetAllBookingsQuery>>(okResult.Value);
+			Assert.Equal(2, returnedBookings.Count());
+		}
+
+		[Fact]
+		public async Task GetAllBookingAsync_ReturnsNotFound_WhenNoBookingsExist()
+		{
+			// Arrange
+			_mediatorMock.Setup(m => m.Send(It.IsAny<GetAllBookingsQuery>(), default)).ReturnsAsync(new List<GetAllBookingsQuery>());
+
+			// Act
+			var result = await _bookingsController.GetAllBookingAsync();
+
+			// Assert
+			Assert.IsType<NotFoundObjectResult>(result);
+		}
+
+		[Fact]
+		public async Task CreateBookingAsync_ReturnsOk_WhenBookingCreated()
+		{
+			// Arrange
+			_bookingsController.ModelState.Clear();
+			var createCommand = new CreateBookingCommand();
+			_mediatorMock.Setup(m => m.Send(It.IsAny<CreateBookingCommand>(), default)).ReturnsAsync(true);
+
+			// Act
+			var result = await _bookingsController.CreateBookingAsync(createCommand);
+
+			// Assert
+			Assert.IsType<OkObjectResult>(result);
+		}
+
+		[Fact]
+		public async Task CreateBookingAsync_ReturnsBadRequest_WhenCommandIsNull()
+		{
+			// Act
+			var result = await _bookingsController.CreateBookingAsync(null);
+
+			// Assert
+			Assert.IsType<BadRequestObjectResult>(result);
+		}
+
+		[Fact]
+		public async Task CreateBookingAsync_ReturnsBadRequest_WhenResultIsNull()
+		{
+			// Arrange
+			_bookingsController.ModelState.Clear();
+			var createCommand = new CreateBookingCommand();
+			_mediatorMock.Setup(m => m.Send(It.IsAny<CreateBookingCommand>(), default)).ReturnsAsync(false);
+
+			// Act
+			var result = await _bookingsController.CreateBookingAsync(createCommand);
+
+			// Assert
+			Assert.IsType<BadRequestObjectResult>(result);
+		}
+
+		[Fact]
+		public async Task DeleteBookingAsync_ReturnsNoContent_WhenBookingDeleted()
+		{
+			// Arrange
+			var bookingId = Guid.NewGuid();
+			_mediatorMock.Setup(m => m.Send(It.IsAny<DeleteBookingCommand>(), default)).ReturnsAsync(true);
+
+			// Act
+			var result = await _bookingsController.DeleteBookingAsync(bookingId);
+
+			// Assert
+			Assert.IsType<NoContentResult>(result);
+		}
+
+		[Fact]
+		public async Task DeleteBookingAsync_ReturnsBadRequest_WhenIdIsEmpty()
+		{
+			// Arrange
+			var bookingId = Guid.Empty;
+
+			// Act
+			var result = await _bookingsController.DeleteBookingAsync(bookingId);
+
+			// Assert
+			Assert.IsType<BadRequestObjectResult>(result);
+		}
+
+		[Fact]
+		public async Task UpdateBookingAsync_ReturnsOk_WhenBookingUpdated()
+		{
+			// Arrange
+			var bookingId = Guid.NewGuid();
+			var updateCommand = new UpdateBookingCommand();
+			_mediatorMock.Setup(m => m.Send(It.IsAny<UpdateBookingCommand>(), default)).ReturnsAsync(true);
+
+			// Act
+			var result = await _bookingsController.UpdateBookingAsync(bookingId, updateCommand);
+
+			// Assert
+			Assert.IsType<OkObjectResult>(result);
+		}
+
+		[Fact]
+		public async Task UpdateBookingAsync_ReturnsNotFound_WhenResultIsNull()
+		{
+			// Arrange
+			var bookingId = Guid.NewGuid();
+			var updateCommand = new UpdateBookingCommand();
+			_mediatorMock.Setup(m => m.Send(It.IsAny<UpdateBookingCommand>(), default)).ReturnsAsync(false);
+
+			// Act
+			var result = await _bookingsController.UpdateBookingAsync(bookingId, updateCommand);
+
+			// Assert
+			Assert.IsType<NotFoundObjectResult>(result);
+		}
+
+		[Fact]
+		public async Task UpdateBookingAsync_ReturnsBadRequest_WhenCommandIsNullOrIdIsEmpty()
+		{
+			// Arrange
+			var bookingId = Guid.Empty;
+
+			// Act
+			var result = await _bookingsController.UpdateBookingAsync(bookingId, null);
+
+			// Assert
+			Assert.IsType<BadRequestObjectResult>(result);
+		}
+
+		[Fact]
+		public async Task ConfirmBookingAsync_ReturnsOk_WhenBookingConfirmed()
+		{
+			// Arrange
+			_bookingsController.ModelState.Clear();
+			var confirmCommand = new ConfirmBookingCommand();
+			_mediatorMock.Setup(m => m.Send(It.IsAny<ConfirmBookingCommand>(), default)).ReturnsAsync(true);
+
+			// Act
+			var result = await _bookingsController.ConfirmBookingAsync(confirmCommand);
+
+			// Assert
+			Assert.IsType<OkObjectResult>(result);
+		}
+
+		[Fact]
+		public async Task ConfirmBookingAsync_ReturnsBadRequest_WhenModelStateIsInvalid()
+		{
+			// Arrange
+			_bookingsController.ModelState.AddModelError("BookingId", "BookingId is required");
+
+			// Act
+			var result = await _bookingsController.ConfirmBookingAsync(new ConfirmBookingCommand());
+
+			// Assert
+			Assert.IsType<BadRequestObjectResult>(result);
+		}
+
+		[Fact]
+		public async Task CompleteBookingAsync_ReturnsOk_WhenBookingCompleted()
+		{
+			// Arrange
+			_bookingsController.ModelState.Clear();
+			var completeCommand = new CompleteBookingCommand();
+			_mediatorMock.Setup(m => m.Send(It.IsAny<CompleteBookingCommand>(), default)).ReturnsAsync(true);
+
+			// Act
+			var result = await _bookingsController.CompleteBookingAsync(completeCommand);
+
+			// Assert
+			Assert.IsType<OkObjectResult>(result);
+		}
+
+		[Fact]
+		public async Task CompleteBookingAsync_ReturnsBadRequest_WhenModelStateIsInvalid()
+		{
+			// Arrange
+			_bookingsController.ModelState.AddModelError("BookingId", "BookingId is required");
+
+			// Act
+			var result = await _bookingsController.CompleteBookingAsync(new CompleteBookingCommand());
+
+			// Assert
+			Assert.IsType<BadRequestObjectResult>(result);
+		}
+
+		[Fact]
+		public async Task AddLineItemToBookingAsync_ReturnsOk_WhenLineItemAdded()
+		{
+			// Arrange
+			var bookingId = Guid.NewGuid();
+			var addLineItemCommand = new AddLineItemToBookingCommand();
+			_mediatorMock.Setup(m => m.Send(It.IsAny<AddLineItemToBookingCommand>(), default)).ReturnsAsync(true);
+
+			// Act
+			var result = await _bookingsController.AddLineItemToBookingAsync(bookingId, addLineItemCommand);
+
+			// Assert
+			Assert.IsType<OkObjectResult>(result);
+		}
+
+		[Fact]
+		public async Task AddLineItemToBookingAsync_ReturnsNotFound_WhenResultIsNull()
+		{
+			// Arrange
+			var bookingId = Guid.NewGuid();
+			var addLineItemCommand = new AddLineItemToBookingCommand();
+			_mediatorMock.Setup(m => m.Send(It.IsAny<AddLineItemToBookingCommand>(), default)).ReturnsAsync(false);
+
+			// Act
+			var result = await _bookingsController.AddLineItemToBookingAsync(bookingId, addLineItemCommand);
+
+			// Assert
+			Assert.IsType<NotFoundObjectResult>(result);
+		}
+
+		[Fact]
+		public async Task AddLineItemToBookingAsync_ReturnsBadRequest_WhenIdOrCommandIsInvalid()
+		{
+			// Arrange
+			var bookingId = Guid.Empty;
+
+			// Act
+			var result = await _bookingsController.AddLineItemToBookingAsync(bookingId, null);
+
+			// Assert
+			Assert.IsType<BadRequestObjectResult>(result);
+		}
+
+		[Fact]
+		public async Task GetBookingByDetailsAsync_ReturnsOk_WhenBookingExists()
+		{
+			// Arrange
+			var detailsQuery = new GetBookingByDetailsQuery();
+			var booking = new GetBookingByDetailsQuery();
+			_mediatorMock.Setup(m => m.Send(It.IsAny<GetBookingByDetailsQuery>(), default)).ReturnsAsync(booking);
+
+			// Act
+			var result = await _bookingsController.GetBookingByDetailsAsync(detailsQuery);
+
+			// Assert
+			var okResult = Assert.IsType<OkObjectResult>(result);
+			Assert.Equal(booking, okResult.Value);
+		}
+
+		[Fact]
+		public async Task GetBookingByDetailsAsync_ReturnsNotFound_WhenBookingDoesNotExist()
+		{
+			// Arrange
+			var detailsQuery = new GetBookingByDetailsQuery();
+			_mediatorMock.Setup(m => m.Send(It.IsAny<GetBookingByDetailsQuery>(), default)).ReturnsAsync((GetBookingByDetailsQuery)null);
+
+			// Act
+			var result = await _bookingsController.GetBookingByDetailsAsync(detailsQuery);
+
+			// Assert
+			Assert.IsType<NotFoundObjectResult>(result);
+		}
+
+		[Fact]
+		public async Task GetBookingByDetailsAsync_ReturnsBadRequest_WhenDetailsIsEmpty()
+		{
+			// Arrange
+			var detailsQuery = new GetBookingByDetailsQuery();
+			_bookingsController.ModelState.AddModelError("PropertyName", "Error message");
+
+			// Act
+			var result = await _bookingsController.GetBookingByDetailsAsync(detailsQuery);
+
+			// Assert
+			Assert.IsType<BadRequestObjectResult>(result);
+		}
+	}
+}
---
+++ CraftConnect.Tests/UserControllerTests.cs
@@ -0,0 +1,396 @@
+﻿using Xunit;
+using Moq;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using CraftConnect.Controllers;
+using CraftConnect.Application.Features.Queries.Users.LoginUser;
+using CraftConnect.Application.Features.Commands.Users.RegisterNewUser;
+using CraftConnect.Application.Features.Commands.Users.ConfirmEmail;
+using CraftConnect.Application.Features.Commands.Users.ChangePassword;
+using CraftConnect.Application.Features.Commands.Users.DeleteUser;
+using CraftConnect.Application.Features.Queries.Users.GetUserById;
+using CraftConnect.Application.Features.Queries.Users.GetAllUsers;
+using CraftConnect.Application.Features.Queries.Users.GetUserByEmail;
+using CraftConnect.Application.Features.Commands.Users.RefreshToken;
+using CraftConnect.Application.DTOs.Users.Commands.RefreshToken;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace CraftConnect.Tests
+{
+	public class UserControllerTest
+	{
+		private readonly Mock<IMediator> _mediatorMock;
+		private readonly UsersController _usersController;
+
+		public UserControllerTest()
+		{
+			_mediatorMock = new Mock<IMediator>();
+			_usersController = new UsersController(_mediatorMock.Object);
+		}
+
+		[Fact]
+		public async Task LoginUserAsync_ReturnsOk_WhenLoginSucceeds()
+		{
+			// Arrange
+			_usersController.ModelState.Clear();
+			var loginCommand = new LoginUserCommand
+			{
+				Username = "testuser",
+				Password = "testpassword"
+			};
+			var accessToken = "access_token_mock";
+			var refreshToken = "refresh_token_mock";
+			_mediatorMock.Setup(m => m.Send(It.IsAny<LoginUserCommand>(), default))
+				.ReturnsAsync((accessToken, refreshToken));
+
+			// Act
+			var result = await _usersController.LoginUserAsync(loginCommand);
+
+			// Assert
+			var okResult = Assert.IsType<OkObjectResult>(result);
+			dynamic returnedTokens = okResult.Value;
+			Assert.Equal(accessToken, returnedTokens.Item1);
+			Assert.Equal(refreshToken, returnedTokens.Item2);
+		}
+
+		[Fact]
+		public async Task LoginUserAsync_ReturnsUnauthorized_WhenLoginFails()
+		{
+			// Arrange
+			_usersController.ModelState.Clear();
+			var loginCommand = new LoginUserCommand
+			{
+				Username = "wronguser",
+				Password = "wrongpassword"
+			};
+			_mediatorMock.Setup(m => m.Send(It.IsAny<LoginUserCommand>(), default))
+				.ReturnsAsync((string.Empty, string.Empty));
+
+			// Act
+			var result = await _usersController.LoginUserAsync(loginCommand);
+
+			// Assert
+			Assert.IsType<UnauthorizedObjectResult>(result);
+		}
+
+		[Fact]
+		public async Task LoginUserAsync_ReturnsBadRequest_WhenModelStateIsInvalid()
+		{
+			// Arrange
+			_usersController.ModelState.AddModelError("Username", "Username is required");
+
+			// Act
+			var result = await _usersController.LoginUserAsync(new LoginUserCommand());
+
+			// Assert
+			Assert.IsType<BadRequestObjectResult>(result);
+		}
+
+		[Fact]
+		public async Task RegisterNewUserAsync_ReturnsOkResult_WhenUserIsRegistered()
+		{
+			// Arrange
+			_usersController.ModelState.Clear();
+			var registerCommand = new RegisterNewUserCommand();
+			_mediatorMock.Setup(m => m.Send(It.IsAny<RegisterNewUserCommand>(), default))
+				.ReturnsAsync(true);
+
+			// Act
+			var result = await _usersController.RegisterNewUserAsync(registerCommand);
+
+			// Assert
+			Assert.IsType<OkObjectResult>(result);
+		}
+
+		[Fact]
+		public async Task RegisterNewUserAsync_ReturnsBadRequest_WhenRegistrationFails()
+		{
+			// Arrange
+			_usersController.ModelState.Clear();
+			var registerCommand = new RegisterNewUserCommand();
+			_mediatorMock.Setup(m => m.Send(It.IsAny<RegisterNewUserCommand>(), default))
+				.ReturnsAsync(false);
+
+			// Act
+			var result = await _usersController.RegisterNewUserAsync(registerCommand);
+
+			// Assert
+			Assert.IsType<BadRequestObjectResult>(result);
+		}
+
+		[Fact]
+		public async Task RegisterNewUserAsync_ReturnsBadRequest_WhenModelStateIsInvalid()
+		{
+			// Arrange
+			_usersController.ModelState.AddModelError("Name", "Name is required");
+
+			// Act
+			var result = await _usersController.RegisterNewUserAsync(new RegisterNewUserCommand());
+
+			// Assert
+			Assert.IsType<BadRequestObjectResult>(result);
+		}
+
+		[Fact]
+		public async Task RegisterNewUserAsync_ReturnsBadRequest_ForMissingUsername()
+		{
+			// Arrange
+			_usersController.ModelState.AddModelError("Username", "Username is missing");
+			var registerCommand = new RegisterNewUserCommand
+			{
+				// Username is not set
+				Email = "test@example.com",
+				Password = "password123",
+				FirstName = "Test",
+				LastName = "User"
+			};
+
+			// Act
+			var result = await _usersController.RegisterNewUserAsync(registerCommand);
+
+			// Assert
+			Assert.IsType<BadRequestObjectResult>(result);
+		}
+
+		[Fact]
+		public async Task ConfirmEmailAsync_ReturnsOk_WhenEmailIsConfirmed()
+		{
+			// Arrange
+			_usersController.ModelState.Clear();
+			var confirmCommand = new ConfirmEmailCommand();
+			_mediatorMock.Setup(m => m.Send(It.IsAny<ConfirmEmailCommand>(), default))
+				.ReturnsAsync(true);
+
+			// Act
+			var result = await _usersController.ConfirmEmailAsync(confirmCommand);
+
+			// Assert
+			Assert.IsType<OkObjectResult>(result);
+		}
+
+		[Fact]
+		public async Task ConfirmEmailAsync_ReturnsBadRequest_WhenEmailConfirmationFails()
+		{
+			// Arrange
+			_usersController.ModelState.Clear();
+			var confirmCommand = new ConfirmEmailCommand();
+			_mediatorMock.Setup(m => m.Send(It.IsAny<ConfirmEmailCommand>(), default))
+				.ReturnsAsync(false);
+
+			// Act
+			var result = await _usersController.ConfirmEmailAsync(confirmCommand);
+
+			// Assert
+			Assert.IsType<BadRequestObjectResult>(result);
+		}
+
+		[Fact]
+		public async Task ConfirmEmailAsync_ReturnsBadRequest_WhenModelStateIsInvalid()
+		{
+			// Arrange
+			_usersController.ModelState.AddModelError("UserId", "UserId is required");
+
+			// Act
+			var result = await _usersController.ConfirmEmailAsync(new ConfirmEmailCommand());
+
+			// Assert
+			Assert.IsType<BadRequestObjectResult>(result);
+		}
+
+		[Fact]
+		public async Task ChangePasswordAsync_ReturnsOk_WhenPasswordIsChanged()
+		{
+			// Arrange
+			_usersController.ModelState.Clear();
+			var changeCommand = new ChangePasswordCommand();
+			_mediatorMock.Setup(m => m.Send(It.IsAny<ChangePasswordCommand>(), default))
+				.ReturnsAsync(true);
+
+			// Act
+			var result = await _usersController.ChangePasswordAsync(changeCommand);
+
+			// Assert
+			Assert.IsType<OkObjectResult>(result);
+		}
+
+		[Fact]
+		public async Task ChangePasswordAsync_ReturnsBadRequest_WhenModelStateIsInvalid()
+		{
+			// Arrange
+			_usersController.ModelState.AddModelError("UserId", "UserId is required");
+
+			// Act
+			var result = await _usersController.ChangePasswordAsync(new ChangePasswordCommand());
+
+			// Assert
+			Assert.IsType<BadRequestObjectResult>(result);
+		}
+
+		[Fact]
+		public async Task RefreshTokenAsync_ReturnsOk_WhenTokenIsRefreshed()
+		{
+			// Arrange
+			_usersController.ModelState.Clear();
+			var refreshTokenDto = new RefreshTokenDto { AccessToken = "oldAccessToken", RefreshToken = "oldRefreshToken" };
+			var newAccessToken = "newAccessToken";
+			var newRefreshToken = "newRefreshToken";
+			_mediatorMock.Setup(m => m.Send(It.IsAny<RefreshTokenCommand>(), default))
+				.ReturnsAsync((newAccessToken, newRefreshToken));
+
+			// Act
+			var result = await _usersController.RefreshTokenAsync(refreshTokenDto);
+
+			// Assert
+			var okResult = Assert.IsType<OkObjectResult>(result);
+			dynamic returnedTokens = okResult.Value;
+			Assert.Equal(newAccessToken, returnedTokens.AccessToken);
+			Assert.Equal(newRefreshToken, returnedTokens.RefreshToken);
+		}
+
+		[Fact]
+		public async Task RefreshTokenAsync_ReturnsUnauthorized_WhenRefreshTokenFails()
+		{
+			// Arrange
+			_usersController.ModelState.Clear();
+			var refreshTokenDto = new RefreshTokenDto { AccessToken = "invalidToken", RefreshToken = "invalidRefreshToken" };
+			_mediatorMock.Setup(m => m.Send(It.IsAny<RefreshTokenCommand>(), default))
+				.ReturnsAsync((string.Empty, string.Empty));
+
+			// Act
+			var result = await _usersController.RefreshTokenAsync(refreshTokenDto);
+
+			// Assert
+			Assert.IsType<UnauthorizedObjectResult>(result);
+		}
+
+		[Fact]
+		public async Task RefreshTokenAsync_ReturnsBadRequest_WhenModelStateIsInvalid()
+		{
+			// Arrange
+			_usersController.ModelState.AddModelError("AccessToken", "AccessToken is required");
+
+			// Act
+			var result = await _usersController.RefreshTokenAsync(new RefreshTokenDto());
+
+			// Assert
+			Assert.IsType<BadRequestObjectResult>(result);
+		}
+
+		[Fact]
+		public async Task GetAllUsersAsync_ReturnsOkResult_WhenUsersExist()
+		{
+			// Arrange
+			var users = new List<GetUserByIdQuery> { new GetUserByIdQuery(), new GetUserByIdQuery() };
+			_mediatorMock.Setup(m => m.Send(It.IsAny<GetAllUsersQuery>(), default)).ReturnsAsync(users);
+
+			// Act
+			var result = await _usersController.GetAllUsersAsync();
+
+			// Assert
+			var okResult = Assert.IsType<OkObjectResult>(result);
+			var returnedUsers = Assert.IsAssignableFrom<IEnumerable<GetUserByIdQuery>>(okResult.Value);
+			Assert.Equal(2, returnedUsers.Count());
+		}
+
+		[Fact]
+		public async Task GetAllUsersAsync_ReturnsNotFound_WhenNoUsersExist()
+		{
+			// Arrange
+			_mediatorMock.Setup(m => m.Send(It.IsAny<GetAllUsersQuery>(), default)).ReturnsAsync(new List<GetUserByIdQuery>());
+
+			// Act
+			var result = await _usersController.GetAllUsersAsync();
+
+			// Assert
+			Assert.IsType<NotFoundObjectResult>(result);
+		}
+
+		[Fact]
+		public async Task GetUserByIdAsync_ReturnsOkResult_WhenUserExists()
+		{
+			// Arrange
+			var userId = Guid.NewGuid();
+			var user = new GetUserByIdQuery();
+			_mediatorMock.Setup(m => m.Send(It.IsAny<GetUserByIdQuery>(), default)).ReturnsAsync(user);
+
+			// Act
+			var result = await _usersController.GetUserByIdAsync(userId);
+
+			// Assert
+			var okResult = Assert.IsType<OkObjectResult>(result);
+			Assert.Equal(user, okResult.Value);
+		}
+
+		[Fact]
+		public async Task GetUserByIdAsync_ReturnsNotFound_WhenUserDoesNotExist()
+		{
+			// Arrange
+			var userId = Guid.NewGuid();
+			_mediatorMock.Setup(m => m.Send(It.IsAny<GetUserByIdQuery>(), default)).ReturnsAsync((GetUserByIdQuery)null);
+
+			// Act
+			var result = await _usersController.GetUserByIdAsync(userId);
+
+			// Assert
+			Assert.IsType<NotFoundObjectResult>(result);
+		}
+
+		[Fact]
+		public async Task GetUserByIdAsync_ReturnsBadRequest_WhenIdIsEmpty()
+		{
+			// Arrange
+			var userId = Guid.Empty;
+
+			// Act
+			var result = await _usersController.GetUserByIdAsync(userId);
+
+			// Assert
+			Assert.IsType<BadRequestObjectResult>(result);
+		}
+
+		[Fact]
+		public async Task GetUserByEmailAsync_ReturnsOkResult_WhenUserExists()
+		{
+			// Arrange
+			var userEmail = "test@example.com";
+			var user = new GetUserByEmailQuery();
+			_mediatorMock.Setup(m => m.Send(It.IsAny<GetUserByEmailQuery>(), default)).ReturnsAsync(user);
+
+			// Act
+			var result = await _usersController.GetUserByEmailAsync(userEmail);
+
+			// Assert
+			var okResult = Assert.IsType<OkObjectResult>(result);
+			Assert.Equal(user, okResult.Value);
+		}
+
+		[Fact]
+		public async Task GetUserByEmailAsync_ReturnsNotFound_WhenUserDoesNotExist()
+		{
+			// Arrange
+			var userEmail = "nonexistent@example.com";
+			_mediatorMock.Setup(m => m.Send(It.IsAny<GetUserByEmailQuery>(), default)).ReturnsAsync((GetUserByEmailQuery)null);
+
+			// Act
+			var result = await _usersController.GetUserByEmailAsync(userEmail);
+
+			// Assert
+			Assert.IsType<NotFoundObjectResult>(result);
+		}
+
+		[Fact]
+		public async Task DeleteUserAsync_ReturnsNoContent_WhenUserIsDeleted()
+		{
+			// Arrange
+			var userId = Guid.NewGuid();
+			_mediatorMock.Setup(m => m.Send(It.IsAny<DeleteUserCommand>(), default)).ReturnsAsync(true);
+
+			// Act
+			var result = await _usersController.DeleteUserAsync(userId);
+
+			// Assert
+			Assert.IsType<NoContentResult>(result);
+		}
+	}
+}
